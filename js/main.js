4087801bf30836afbd35acd9cad02342ec4ba6e6
PoisoN1991 <ihor.otchenash@gmail.com>
2020-04-26 17:30:51 +0200

    Implemented timer, notifications and small adjustments/fixes

--- js/main.js
+++ js/main.js
@@ -23,10 +23,52 @@ const writePomodors = (data) => {
     `
     pomodorosContainer.appendChild(pomodor);
   }
-  loader.style.display = "none";
+  loader.parentNode.removeChild(loader);
 }
 
 const logError = (error) => console.error(error);
+
+const askPermissionNotification = () => {
+  const handlePermission = (permission) => {
+    if(!('permission' in Notification)) {
+      Notification.permission = permission;
+    }
+
+    if (Notification.permission === 'granted') {
+      document.querySelector('.notifications').innerHTML = "You've allowed the notifications";
+    } else {
+      document.querySelector('.notifications').innerHTML = "You've restricted the notifications. The app won't work as expected";
+    }
+    document.querySelector('.notifications').style.display = "block";
+  }
+
+  const chechNotificationType = () => {
+    try {
+      Notification.requestPermission().then();
+    } catch(error) {
+      return false;
+    }
+    return true;
+  }
+  
+  document.querySelector('.notifications').style.display = "none"
+
+  if (!('Notification' in window)) {
+    document.querySelector('.notifications').innerHTML = "Your browser doesn't support notifications. The app won't work as expected";
+    document.querySelector('.notifications').style.display = "block";
+  } else {
+    if(chechNotificationType()) {
+      Notification.requestPermission()
+      .then((permission) => {
+        handlePermission(permission);
+      })
+    } else {
+      Notification.requestPermission((permission) => {
+        handlePermission(permission);
+      });
+    }
+  }
+}
 
 const addPomodor = () => {
   const pomodor = document.createElement('li');
@@ -42,9 +84,63 @@ const addPomodor = () => {
   pomodorsValue.set(pomodorsAmount + 1);
 }
 
+const startTimer = () => {
+  timer.innerHTML = new Date(timeLeft * 1000).toISOString().substr(11, 8);
+  if (!timerBlocked) {
+    timerBlocked = true;
+    const startTimer = window.setInterval(() => {
+    if (timerStart === timerStop - 1) {
+      clearInterval(startTimer);
+      timerStart = 0;
+      timeLeft = timerStop - timerStart;
+      'Notification' in window
+        ? Notification.permission === "granted" ? sendNotification() : callNotificationPopup()
+        : callNotificationPopup();
+      addPomodor();
+      document.querySelector(".header__add-pomodoro").innerHTML = "Start Tracking";
+      return timerBlocked = false;
+    }
+    timeLeft -= 1;
+    timerStart += 1;
+    timer.innerHTML = new Date(timeLeft * 1000).toISOString().substr(11, 8);
+  }, 1000);
+  } else {
+    return;
+  }
+}
+
+const callNotificationPopup = () => {
+  const notificationPopup = document.querySelector(".notification-container");
+  notificationPopup.style.display = "block";
+  timer.innerHTML = new Date(timeLeft * 1000).toISOString().substr(11, 8);
+}
+
+const closeNotificationPopup = () => {
+  const notificationPopup = document.querySelector(".notification-container");
+  notificationPopup.style.display = "none";
+}
+
+const sendNotification = () => {
+  const img = '/img/tomato.png';
+  const text = 'Take a break! You did your 25 minutes!';
+  const notification = new Notification('Pomodoro App', { body: text, icon: img});
+  timer.innerHTML = new Date(timeLeft * 1000).toISOString().substr(11, 8);
+}
+
+document.querySelector('.notification-popup__close').addEventListener('click', () => {
+  closeNotificationPopup();
+});
+
+const timer = document.querySelector('.timer');
+let timerBlocked = false;
+let timerStart = 0;
+const timerStop = 10;
+let timeLeft = timerStop - timerStart;
+
+askPermissionNotification();
 const database = firebase.database();
 const timeSpentValue = database.ref('timeSpent');
 timeSpentValue.once('value', writeTimeSpent, logError);
 const pomodorsValue = database.ref('pomodors');
 pomodorsValue.once('value', writePomodors, logError);
-document.querySelector('.header__add-pomodoro').onclick = addPomodor;
+document.querySelector('.header__add-pomodoro').onclick = startTimer;
